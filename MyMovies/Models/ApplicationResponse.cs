659df61fac55d53aa43af15f8d734ed6a966308f
kamryn-prock <90482936+kamryn-prock@users.noreply.github.com>
2022-02-02 15:19:07 -0700

    Mission 4

---
+++ MyMovies/Models/ApplicationResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovies.Models
+{
+    public class ApplicationResponse
+    {
+        // category, title, year, director, rating, edited, lent to, notes
+        // rating field with a dropdown menu
+        // edited field to be yes/no
+        // edited, lent to, and notes are optional. Notes limited to 25 characters
+        
+        [Key]
+        [Required]
+        public int ApplicationId { get; set; }
+        [Required]
+        public string category { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public short year { get; set; }
+        [Required]
+        public string director { get; set; }
+        [Required]
+        public string rating { get; set; } // make a drop down
+        public bool edited { get; set; }
+        public string lent { get; set; }
+        
+        //[Range(0, 25)]
+        public string notes { get; set; }// 25 characters
+        
+    }
+}
